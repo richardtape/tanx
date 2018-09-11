fd6b75de5cde4ebdbecc2ec725d0b6b21128f68a
Richard Tape <richard.tape@ubc.ca>
2018-09-11 14:32:11 -0700

    CHANGED read more link for excerpts to be on new line (and tweaked formating for code standards)

--- content.php
+++ content.php
@@ -6,18 +6,23 @@
 <?php } ?>
 <div class="post-content">
 	<?php
+
 	$more_string = __( 'Continue reading', 'tanx' );
-	if ( ! is_single() && has_excerpt( $post->ID ) ) { ?>
-	<p>
+
+	if ( ! is_single() && has_excerpt( $post->ID ) ) {
+		?>
+
+		<p><?php echo wp_kses_post( get_the_excerpt() ); ?></p>
+		<p class="pc-excerpt">
+			<a href="<?php the_permalink(); ?>" class="more-link"><?php echo wp_kses_post( $more_string ); ?></a>
+		</p>
 		<?php
-		echo get_the_excerpt();
-		?> <a href="<?php the_permalink(); ?>" class="more-link"><?php echo $more_string; ?></a>
-	</p> <?php
-}
-else
-	the_content($more_string);
-	if(shortcode_exists('[ssba_hide]')) {
-		echo do_shortcode("[ssba_hide]");
+	} else {
+		the_content( $more_string );
 	}
-?>
+
+	if ( shortcode_exists( '[ssba_hide]' ) ) {
+		echo do_shortcode( '[ssba_hide]' );
+	}
+	?>
 </div>
